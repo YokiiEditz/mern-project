a0c5753a05188b42465bce07b2390dff25c2a0a9
Yoki <thekingmaker0012@gmail.com>
2024-10-16 23:21:50 +0530

    server.js changes

--- backend/server.js
+++ backend/server.js
@@ -33,16 +33,22 @@ app.use("/api/products", productRoutes);
 
 // --------------------Deployment---------------
 
-const path = require("path"); //path module
-const __dirname1 = path.resolve();
+// const path = require("path"); //path module
+// const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "..", "frontend", "dist")));
+  // app.use(express.static(path.join(__dirname1, "..", "frontend", "dist")));
 
+  // app.get("*", (req, res) => {
+  //   res.sendFile(
+  //     path.resolve(__dirname1, "..", "frontend", "dist", "index.html")
+  //   );
+  // });
+  const path = require("path");
+
+  app.use(express.static(path.join(__dirname, "..", "frontend", "dist")));
   app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname1, "..", "frontend", "dist", "index.html")
-    );
+    res.sendFile(path.join(__dirname, "..", "frontend", "dist", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
